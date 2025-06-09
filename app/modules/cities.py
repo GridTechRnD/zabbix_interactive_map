2d55b22b2c2320f4aa7bf67d20f536490479482b
gzeloni2 <gustavo.zeloni@gridtech.com.br>
2025-06-09 18:05:24 -0300

    DELETE obsolete dictionary (hard coded)

--- app/modules/cities.py
+++
@@ -1,7 +0,0 @@
-# Define a dictionary to map city names to their respective IDs
-# This dictionary is used to filter hosts by city in the Zabbix API queries
-# The keys are city names and the values are lists of group IDs associated with those cities
-CITIES = dict(
-    prudentopolis=[219],
-    lapa=[161]
-)
